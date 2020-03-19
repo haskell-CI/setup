9a3c91b3cd9638735ec338061d3d0360d2e0354d
Jared Weakly <jweakly@galois.com>
2020-03-18 21:14:45 -0700

    Try reconfiguring tsconfig to fix CI run error

--- src/setup-haskell.ts
+++ src/setup-haskell.ts
@@ -4,16 +4,21 @@ import {safeLoad} from 'js-yaml';
 import {readFileSync} from 'fs';
 import {join} from 'path';
 
-const actionYml = safeLoad(readFileSync(join('..', 'action.yml'), 'utf8'));
+const actionYml = safeLoad(
+  readFileSync(join(__dirname, '..', 'action.yml'), 'utf8')
+);
 const defaultGHCVersion = actionYml.inputs['ghc-version'].default;
 const defaultCabalVersion = actionYml.inputs['cabal-version'].default;
 
 async function run(): Promise<void> {
   try {
+    core.info('Preparing to setup GHC and Cabal');
     const ghcVersion = core.getInput('ghc-version') || defaultGHCVersion;
+    core.info(`Installing GHC version ${ghcVersion}`);
     await installGHC(ghcVersion);
 
     const cabalVersion = core.getInput('cabal-version') || defaultCabalVersion;
+    core.info(`Installing Cabal version ${cabalVersion}`);
     await installCabal(cabalVersion);
   } catch (error) {
     core.setFailed(error.message);
