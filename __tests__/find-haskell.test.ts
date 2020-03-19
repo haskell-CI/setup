4ad742ea57c3c18f5a15fd69e9897d2da494952e
Jared Weakly <jweakly@galois.com>
2020-03-18 20:11:57 -0700

    Implement initial simplistic test suite

--- __tests__/find-haskell.test.ts
+++ __tests__/find-haskell.test.ts
@@ -1,6 +1,6 @@
 import * as io from '@actions/io';
-import fs = require('fs');
-import path = require('path');
+import * as fs from 'fs';
+import * as path from 'path';
 
 const toolDir = path.join(__dirname, 'runner', 'tools');
 
@@ -8,55 +8,20 @@ process.env['AGENT_TOOLSDIRECTORY'] = toolDir;
 process.env['RUNNER_TOOL_CACHE'] = toolDir;
 
 import {installGHC, installCabal} from '../src/installer';
+import {safeLoad} from 'js-yaml';
 
-describe('find-haskell', () => {
-  beforeAll(async () => {
-    await io.rmRF(toolDir);
-
-    const ghcDir: string = path.join(toolDir, 'ghc', '8.6.5', 'bin');
-    await io.mkdirP(ghcDir);
-    fs.writeFileSync(`${ghcDir}.complete`, 'hello');
-
-    const cabalDir: string = path.join(toolDir, 'cabal', '2.0', 'bin');
-    await io.mkdirP(cabalDir);
-    fs.writeFileSync(`${cabalDir}.complete`, 'hello');
+describe('haskell-ci/setup-haskell', () => {
+  it('Parses action.yml to get correct default GHC', async () => {
+    const actionYml = safeLoad(
+      fs.readFileSync(path.join(__dirname, '..', 'action.yml'), 'utf8')
+    );
+    expect(actionYml.inputs['ghc-version'].default).toBe('8.8.3');
   });
 
-  afterAll(async () => {
-    try {
-      await io.rmRF(toolDir);
-    } catch {
-      console.log('Failed to remove test directories');
-    }
-  }, 100000);
-
-  it('Uses version of ghc installed in cache', async () => {
-    // This will throw if it doesn't find it in the cache (because no such version exists)
-    await installGHC('8.6.5');
-  });
-
-  it('Uses version of cabal installed in cache', async () => {
-    // This will throw if it doesn't find it in the cache (because no such version exists)
-    await installCabal('2.0');
-  });
-
-  it('findHaskellGHCVersion throws if cannot find any version of ghc', async () => {
-    let thrown = false;
-    try {
-      await installGHC('0.0.0');
-    } catch {
-      thrown = true;
-    }
-    expect(thrown).toBe(true);
-  });
-
-  it('findHaskellCabalVersion throws if cannot find any version of ghc', async () => {
-    let thrown = false;
-    try {
-      await installCabal('0.0.0');
-    } catch {
-      thrown = true;
-    }
-    expect(thrown).toBe(true);
+  it('Parses action.yml to get correct default Cabal', async () => {
+    const actionYml = safeLoad(
+      fs.readFileSync(path.join(__dirname, '..', 'action.yml'), 'utf8')
+    );
+    expect(actionYml.inputs['cabal-version'].default).toBe('3.0.0.0');
   });
 });
