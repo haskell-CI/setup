8a88d17fe370b55a6ad2cc77ca4daa7c953e4a4f
Jared Weakly <jweakly@galois.com>
2020-04-02 12:25:39 -0700

    Fix missing tsconfig in lintstagedrc

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   '!(*test).{js,ts}': 'eslint --cache --fix',
   '!(*test).ts': () => ['ncc build', 'git add dist'],
-  'src/**/*.ts': ['tsc'],
+  'src/**/*.ts': () => 'tsc -p tsconfig.json',
   '*.{js,ts,json,md}': 'prettier --write'
 };
